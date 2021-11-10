b6bba13629ca50f2abdd3b925aa8258d4b6e6020
Thomas4897 <t.maynard4897@gmail.com>
2021-11-11 05:24:02 +0800

    Adding CSS and Adding TODO list Function

---
+++ main.js
@@ -0,0 +1,37 @@
+const todoForm = document.querySelector("#todo-form");
+
+const todoInputText = document.querySelector("#todo-input-text");
+
+const todoList = document.querySelector("#todo-list");
+
+const addTodoSubmit = document.querySelector("#add-todo-submit");
+const removeCompletedSubmit = document.querySelector(
+	"#remove-completed-submit"
+);
+const removeAllSubmit = document.querySelector("#remove-all-submit");
+
+addTodoSubmit.value = "ADD";
+removeCompletedSubmit.value = "REMOVE COMPLETED TASKS";
+removeAllSubmit.value = "REMOVE ALL";
+
+todoForm.addEventListener("submit", function (event) {
+	event.preventDefault();
+	console.log("The todoForm function called");
+	let str = todoInputText.value;
+	const newDiv = document.createElement("div");
+	newDiv.innerHTML = str;
+
+	newDiv.addEventListener("click", function () {
+		if (newDiv.style.textDecoration === "") {
+			newDiv.style.textDecoration = "line-through";
+		} else {
+			newDiv.style.textDecoration = "";
+		}
+	});
+
+	if (str !== "") {
+		todoList.appendChild(newDiv);
+	}
+
+	todoForm.reset();
+});
